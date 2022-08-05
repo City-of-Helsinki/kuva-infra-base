9773bda44fc0b3562fe4bb5794a226cb82b1629f
Tero Virtanen <tero.virtanen@mavericks.fi>
2022-08-05 13:12:22 +0300

    list pods on same namespace

--- maintain/kube_pod_cleaner/pod_cleaner.sh
+++ maintain/kube_pod_cleaner/pod_cleaner.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-# Loop through all Namespaces
 echo "run pod_cleaner"
+
+# lsit pods in current namespace
+kubectl get pod
+
+# Loop through all Namespaces
 for namespace in $(kubectl get namespace -o=json |  jq -r '.items[] .metadata.name') ; do
   echo "Processing namespace ${namespace}"
 done
