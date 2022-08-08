49d1af04f0ebd0473859f3cbf175126f58bca8cc
Tero Virtanen <tero.virtanen@mavericks.fi>
2022-08-08 13:09:53 +0300

    list failed pods

--- maintain/kube_pod_cleaner/pod_cleaner.sh
+++ maintain/kube_pod_cleaner/pod_cleaner.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 echo "run pod_cleaner"
 
-# lsit pods in current namespace
+# list pods in current namespace
 kubectl get pod
 
 # Loop through all Namespaces
 for namespace in $(kubectl get namespace -o=json |  jq -r '.items[] .metadata.name') ; do
   echo "Processing namespace ${namespace}"
 done
+
+#  failed pods to clean
+kubectl get po --field-selector status.phase=Failed --all-namespaces --no-headers
+
+# clean failed pods
+#kubectl get po --field-selector status.phase=Failed --all-namespaces --no-headers | awk '{print $2 " --namespace=" $1}' | xargs -n2 kubectl delete pod
